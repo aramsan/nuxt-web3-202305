3f12e82925b102fd253932f3f3a3b528f5539576
aram.mine <aram.mine@gaiax.com>
2023-05-20 01:51:24 +0900

    first contract

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,48 @@
 <template>
-  <Tutorial/>
+  <div>
+    <p>
+      <input
+        type="text"
+        v-model="inputNumber"
+        placeholder="input number"
+      >
+      <button
+        @click="setNumber()"
+      >
+        Set Number to contract
+      </button>
+      </p>
+      <p>
+      <button
+        @click="getNumber()"
+      >
+        Get Number from contract
+      </button>
+        Number:{{number}}
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      number: 0, // コントラクトから取得する数値
+      inputNumber: 0 // フォームから入力された数値
+    }
+  },
+  methods: {
+    getNumber: async function() {
+      let ret = await this.$contract.methods.get().call() // コントラクトからの読み込み部分
+      this.number = ret // フロントへ反映
+    },
+      setNumber: async function() {
+      let accounts = await this.$web3.eth.getAccounts() // MetaMask で使っているアカウントの取得
+      let account = accounts[0]
+      let ret = await this.$contract.methods.set(this.inputNumber).send({from: account}) // コントラクトへの書き込み部分
+    },
+  },
   mounted() {
     console.log('Current Block Number')
     this.$web3.eth.getBlockNumber().then(console.log)
--- plugins/web3.js
+++ plugins/web3.js
@@ -1,8 +1,32 @@
 import Web3 from "web3"
+import artifacts from "~~/build/contracts/SingleNumRegister.json"// コントラクトのコンパイル後の設定ファイルの読み込み
 
 export default async function(context, inject) {
-    const httpEndpoint = "http://127.0.0.1:7545" //ここを変えればいろんなチェーンに繋げられます
-    const web3 = new Web3(new Web3.providers.HttpProvider(httpEndpoint))
+    let web3
+    if (
+        typeof window !== 'undefined' &&
+        typeof window.ethereum !== 'undefined'
+    ) {
+        web3 = new Web3(window.ethereum)
+        window.ethereum.enable().catch((error) => {
+        // User denied account access
+        console.log(error)
+        })
+    } else if (
+        typeof window !== 'undefined' &&
+        typeof window.web3 !== 'undefined'
+    ) {
+        web3 = new Web3(window.web3.currentProvider)
+    } else {
+        const httpEndpoint = 'http://127.0.0.1:7545'
+        web3 = new Web3(new Web3.providers.HttpProvider(httpEndpoint))
+    }
 
+    let networkId = await web3.eth.net.getId()// チェーンのネットワーク ID を取得
+    let contract = new web3.eth.Contract( //コントラクトのインスタンスの初期化。設定ファイルと、アドレスが必要
+        artifacts.abi, // コントラクトのコンパイル後の設定ファイル
+        artifacts.networks[networkId].address // ネットワーク ID ごとに保存されているコントラクトのアドレスを読み込む
+    )
+    inject('contract',contract)
     inject('web3',web3)
 }
