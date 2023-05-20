bfd8222b67a1cdf544e4d827507f56d50590ae55
aram.mine <aram.mine@gaiax.com>
2023-05-20 10:59:09 +0900

    gitignoreの更新

--- truffle-config.js
+++ truffle-config.js
@@ -44,6 +44,7 @@
 // require('dotenv').config();
 // const { MNEMONIC, PROJECT_ID } = process.env;
 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
@@ -89,7 +90,18 @@ module.exports = {
     //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
-    //
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          'https://sepolia.infura.io/v3/' + process.env.INFURA_PROJECT_ID
+        ),
+      network_id: 11155111, // Sepolia's id
+      gas: 5500000 // Ropsten has a lower block limit than mainnet
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    }
     // Useful for private networks
     // private: {
     //   provider: () => new HDWalletProvider(MNEMONIC, `https://network.io`),
